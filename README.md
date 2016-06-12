405eac85ef9ccec93700572e5ce94e4a9e2e8bfe
Ivan Montiel <idmontie@asu.edu>
2016-06-12 15:19:59 -0700

    Setup resources

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # botly-rust
 App written using Rust and React
+
+
+On a Mac?
+
+```
+brew install openssl
+export OPENSSL_INCLUDE_DIR=/usr/local/opt/openssl/include
+export DEP_OPENSSL_INCLUDE=/usr/local/opt/openssl/include
+```
--- src/http/home.rs
+++ src/http/home.rs
@@ -1,9 +1,19 @@
 extern crate iron;
+extern crate hyper;
 extern crate router;
 
 use iron::{Request, Response, IronResult};
 use iron::status;
+use self::hyper::mime::Mime;
+use std::io::Read;
+use std::fs::File;
 
-pub fn get(req: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Home")))
+pub fn get(_: &mut Request) -> IronResult<Response> {
+    let mut f = File::open("resources/index.html").unwrap();
+    let mut s = String::new();
+    f.read_to_string(&mut s).unwrap();
+
+    let content_type = "text/html".parse::<Mime>().unwrap();
+    Ok(Response::with((content_type, status::Ok, s)))
+
 }
